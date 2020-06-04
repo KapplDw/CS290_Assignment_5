e6dd39cc601441bdb68341d4292337871c62dc78
KapplDw <55997890+KapplDw@users.noreply.github.com>
2020-06-03 21:47:27 -0700

    Finished trying to upload all files to git hub

--- server.js
+++ server.js
@@ -2,20 +2,47 @@
  * Write your routing code in this file.  Make sure to add your name and
  * @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name: Dwight Kappl
+ * Email: kappldw@oregonstate.edu
+ *
  */
 
 var path = require('path');
 var express = require('express');
+var express_hdbars = require('express-handlebars')
+var twitData = require('./twitData')
+var single = true;
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+
+app.engine('handlebars', express_hdbars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 app.use(express.static('public'));
 
+// Displays homepage with all twits loaded
+
+app.get( '/', function (req, res, next){
+  res.status(200).render('home', {allTwits: twitData, single});
+});
+
+app.get( '/twits/:indexTwit', function (req, res, next){
+
+  if(twitData[req.params.indexTwit]){
+    res.status(200).render('home', {allTwits: [twitData[req.params.indexTwit]], button: 'single' }  );
+
+  }
+  else{
+    next();
+  }
+});
+
 app.get('*', function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(port, function () {
